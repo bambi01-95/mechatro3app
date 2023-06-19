41f620c4dfebd327b82e84142507da2578943952
bambi01-95 <bambi0905hs@ezweb.ne.jp>
2023-06-19 09:52:42 +0900

    Initial Commit

---
+++ mechatro3/mechatro3App.swift
@@ -0,0 +1,17 @@
+//
+//  mechatro3App.swift
+//  mechatro3
+//
+//  Created by Hiroto SHIKADA on 2023/06/19.
+//
+
+import SwiftUI
+
+@main
+struct mechatro3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
